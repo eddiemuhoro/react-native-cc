7012e34508a55e2f54e9f0df92c12ff3a83ffe7e
eddiemuhoro <eddiemuhoro@gmail.com>
2023-04-26 02:12:38 +0300

    added navigation container

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './components/Home';
+import AuthNavigator from './navigations/AuthNavigator';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Home />
-    </View>
+    <NavigationContainer>
+      <AuthNavigator />
+    </NavigationContainer>
+
+   
   );
 }
 
